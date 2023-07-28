182a56a44e817cb083854b1e06cfdbf63d2bc694
HXCoderr <138549820+HXCoderr@users.noreply.github.com>
2023-07-28 16:03:18 +0530

    edit

---
+++ JS/trailer.js
@@ -0,0 +1,8 @@
+/*document.addEventListener("mousemove", (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+    const pageWidth = Math.max(html.clientHeight);
+    const pageHeight = Math.max(html.clientWidth)
+    const percentPosX = (posX / pageWidth) *;
+
+});*/
