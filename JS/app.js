fb2a05e43ca988544157cfb98d19e2863d82b78c
Kaustubh Mathur <kaustubhmathur123@gmail.com>
2023-07-31 20:13:17 +0400

    Contact

--- JS/app.js
+++ JS/app.js
@@ -41,6 +41,36 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   };
 
 
+// menu
+
+
+
+const navbar = document.querySelector('.navbar');
+const button = document.querySelector('.button');
+
+
+let isNavbarExpanded = false;
+
+
+function toggleNavbar() {
+  if (isNavbarExpanded) {
+
+    navbar.style.height = '0';
+    navbar.style.width = '0';
+  } else {
+
+    navbar.style.height = '100vh';
+    navbar.style.width = '100vw';
+  }
+
+  isNavbarExpanded = !isNavbarExpanded;
+}
+
+
+button.addEventListener('click', toggleNavbar);
+
+
+
 
 
 
